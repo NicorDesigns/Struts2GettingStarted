4c036546385eead2e9fd209ae3edcb8c9ca6b190
Nicolaas Kock <nicocpk@gmail.com>
2019-11-20 14:34:35 -0500

    New Commit

---
+++ struts2-hello-world/src/com/nicordesigns/helloworld/model/MessageStore.java
@@ -0,0 +1,15 @@
+package com.nicordesigns.helloworld.model;
+
+public class MessageStore {
+	
+private String message;
+    
+    public MessageStore() {
+        message = "Hello Struts 2 User :";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
